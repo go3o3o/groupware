c59319bf637fd158458a051be21413c2df7000b6
kimhoyeon <yoni@gimhoyeon-ui-MacBook-Pro.local>
2019-02-07 18:23:36 +0900

    oracle sql statement upload

---
+++ SQL/GroupWare_CreateTable.sql
@@ -0,0 +1,14 @@
+
+@createTable_company.sql 
+@createTable_document.sql 
+@createTable_board.sql 
+
+SELECT * FROM TAB;
+PURGE RECYCLEBIN;
+
+GRANT REFERENCES ON EMPLOYEE TO SYSTEM; 
+
+SELECT * FROM USER_CONS_COLUMNS
+WHERE TABLE_NAME = 'COMPANY';
+
+SELECT * FROM SYSTEM.AUTHORITY;
